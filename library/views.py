fa37415936e34717e8dce24c783276a26aa7f216
zMarcoos <marcosvdcdef@gmail.com>
2025-02-06 02:04:10 -0300

    Toques finais

--- library/views.py
+++ library/views.py
@@ -2,185 +2,196 @@ import os
 import logging
 from django.shortcuts import redirect, render
 from django.http import HttpResponseRedirect
-from . import forms,models
+from . import forms, models
 from django.http import HttpResponseRedirect
 from django.contrib.auth.models import Group
-from django.contrib.auth.decorators import login_required,user_passes_test
+from django.contrib.auth.decorators import login_required, user_passes_test
 from datetime import date
 from django.core.mail import send_mail
 from librarymanagement.settings import EMAIL_HOST_USER
+from django.contrib.auth import logout
+from django.shortcuts import redirect
 
 logger = logging.getLogger(__name__)
 
-#VER ISSO AQUI AQQ
-def is_authenticated(request) -> object:
-    if request.user.is_authenticated: return HttpResponseRedirect('afterlogin')
+
+def custom_logout_view(request):
+    logout(request)
+    return redirect('/')
+
 
 def home_view(request):
     if request.user.is_authenticated:
         return HttpResponseRedirect('afterlogin')
 
-    return render(request,'library/index.html')
+    return render(request, 'library/index.html')
 
-def studentclick_view(request):
+
+def student_click_view(request):
     if request.user.is_authenticated:
         return HttpResponseRedirect('afterlogin')
 
-    return render(request,'library/studentclick.html')
+    return render(request, 'library/studentclick.html')
 
-def adminclick_view(request):
+
+def admin_click_view(request):
     if request.user.is_authenticated:
         return HttpResponseRedirect('afterlogin')
-    return render(request,'library/adminclick.html')
+    return render(request, 'library/adminclick.html')
 
-def studentsignup_view(request):
-    form1=forms.StudentUserForm()
-    form2=forms.StudentExtraForm()
-    mydict={'form1':form1,'form2':form2}
-    if request.method=='POST':
-        form1=forms.StudentUserForm(request.POST)
-        form2=forms.StudentExtraForm(request.POST)
-        if form1.is_valid() and form2.is_valid():
-            user=form1.save()
-            user.set_password(user.password)
-            user.save()
-            f2=form2.save(commit=False)
-            f2.user=user
-            f2.save()
 
-            my_student_group = Group.objects.get_or_create(name='STUDENT')
-            my_student_group[0].user_set.add(user)
+def student_signup_view(request):
+    first_form = forms.StudentUserForm(request.POST or None)
+    second_form = forms.StudentExtraForm(request.POST or None)
+    if request.method == 'POST' and first_form.is_valid() and second_form.is_valid():
+        student_account = first_form.save()
+        student_account.set_password(student_account.password)
+        student_account.save()
+
+        student_extra_data = second_form.save(commit=False)
+        student_extra_data.user = student_account
+        student_extra_data.save()
+
+        my_student_group, _ = Group.objects.get_or_create(name='STUDENT')
+        my_student_group.user_set.add(student_account)
 
         return HttpResponseRedirect('studentlogin')
-    return render(request,'library/studentsignup.html',context=mydict)
-
-
+    return render(request, 'library/studentsignup.html', {'form1': first_form, 'form2': second_form})
 
 
 def is_admin(user):
-    if user.is_superuser or user.is_staff:
-        return True
-    else:
-        return False
+    return user.is_superuser or user.is_staff
+
 
 def is_student(user):
     return user.groups.filter(name='STUDENT').exists()
-def afterlogin_view(request):
+
+
+def after_login_view(request):
     if is_admin(request.user):
-        return render(request,'library/adminafterlogin.html')
-
-    elif(is_student(request.user)):
-        return render(request,'library/studentafterlogin.html')
-
-
-@login_required(login_url='adminlogin')
-@user_passes_test(is_admin)
-def addbook_view(request):
-    #now it is empty book form for sending to html
-    form=forms.BookForm()
-    if request.method=='POST':
-        #now this form have data from html
-        form=forms.BookForm(request.POST)
-        if form.is_valid():
-            form.save()
-            return render(request,'library/bookadded.html')
-    return render(request,'library/addbook.html',{'form':form})
-
-@login_required(login_url='adminlogin')
-@user_passes_test(is_admin)
-def viewbook_view(request):
-    books=models.Book.objects.all()
-    return render(request,'library/viewbook.html',{'books':books})
-
-
+        return render(request, 'library/adminafterlogin.html')
+    elif is_student(request.user):
+        return render(request, 'library/studentafterlogin.html')
 
 
 @login_required(login_url='adminlogin')
 @user_passes_test(is_admin)
-def issuebook_view(request):
-    form=forms.IssuedBookForm()
-    if request.method=='POST':
-        #now this form have data from html
-        form=forms.IssuedBookForm(request.POST)
-        if form.is_valid():
-            obj=models.IssuedBook()
-            obj.enrollment=request.POST.get('enrollment2')
-            obj.isbn=request.POST.get('isbn2')
-            obj.save()
-            return render(request,'library/bookissued.html')
-    return render(request,'library/issuebook.html',{'form':form})
+def add_book_view(request):
+    form = forms.BookForm(request.POST or None)
+    if request.method == 'POST' and form.is_valid():
+        form.save()
+        return render(request, 'library/bookadded.html')
+
+    return render(request, 'library/addbook.html', {'form': form})
 
 
 @login_required(login_url='adminlogin')
 @user_passes_test(is_admin)
-def viewissuedbook_view(request):
-    issuedbooks=models.IssuedBook.objects.all()
-    li=[]
-    for ib in issuedbooks:
-        issdate=str(ib.issuedate.day)+'-'+str(ib.issuedate.month)+'-'+str(ib.issuedate.year)
-        expdate=str(ib.expirydate.day)+'-'+str(ib.expirydate.month)+'-'+str(ib.expirydate.year)
-        #fine calculation
-        days=(date.today()-ib.issuedate)
-        print(date.today())
-        d=days.days
-        fine=0
-        if d>15:
-            day=d-15
-            fine=day*10
+def view_book_view(request):
+    books = models.Book.objects.all()
+    return render(request, 'library/viewbook.html', {'books': books})
 
-
-        books=list(models.Book.objects.filter(isbn=ib.isbn))
-        students=list(models.StudentExtra.objects.filter(enrollment=ib.enrollment))
-        i=0
-        for _ in books:
-            t=(students[i].get_name,students[i].enrollment,books[i].name,books[i].author,issdate,expdate,fine,ib.status)
-            i=i+1
-            li.append(t)
-
-    return render(request,'library/viewissuedbook.html',{'li':li})
 
 @login_required(login_url='adminlogin')
 @user_passes_test(is_admin)
-def viewstudent_view(request):
-    students=models.StudentExtra.objects.all()
-    return render(request,'library/viewstudent.html',{'students':students})
+def issue_book_view(request):
+    form = forms.IssuedBookForm(request.POST or None)
+    if request.method == 'POST' and form.is_valid():
+        models.IssuedBook.objects.create(
+            enrollment=request.POST.get('enrollment2'),
+            isbn=request.POST.get('isbn2')
+        )
+        return render(request, 'library/bookissued.html')
+
+    return render(request, 'library/issuebook.html', {'form': form})
+
+
+@login_required(login_url='adminlogin')
+@user_passes_test(is_admin)
+def view_issued_book_view(request):
+    issued_books = models.IssuedBook.objects.all()
+    issued_books_list = []
+
+    for issued_book in issued_books:
+        issued_book_date = issued_book.issuedate.strftime("%d/%m/%Y")
+        expiry_date = issued_book.expirydate.strftime("%d/%m/%Y")
+
+        days = (date.today() - issued_book.issuedate).days
+        fine = max(0, (days - 15) * 10) if days > 15 else 0
+
+        books = models.Book.objects.filter(isbn=issued_book.isbn)
+        students = models.StudentExtra.objects.filter(
+            enrollment=issued_book.enrollment)
+
+        for book, student in zip(books, students):
+            issued_books_list.append((
+                student.get_name,
+                student.enrollment,
+                book.name,
+                book.author,
+                issued_book_date,
+                expiry_date,
+                fine,
+                issued_book.status
+            ))
+
+    return render(request, 'library/viewissuedbook.html', {'li': issued_books_list})
+
+
+@login_required(login_url='adminlogin')
+@user_passes_test(is_admin)
+def view_student_view(request):
+    students = models.StudentExtra.objects.all()
+    return render(request, 'library/viewstudent.html', {'students': students})
 
 
 @login_required(login_url='studentlogin')
-def viewissuedbookbystudent(request):
+def view_issued_book_by_student(request):
     student = models.StudentExtra.objects.filter(user=request.user).first()
 
     if not student:
         return render(request, 'library/viewissuedbookbystudent.html', {'combined_data': []})
 
-    issued_books = models.IssuedBook.objects.filter(enrollment=student.enrollment)
+    issued_books = models.IssuedBook.objects.filter(
+        enrollment=student.enrollment)
     combined_data = []
 
     for issued_book in issued_books:
         book = models.Book.objects.filter(isbn=issued_book.isbn).first()
 
         if book:
-            issdate = issued_book.issuedate.strftime('%d-%m-%Y')
-            expdate = issued_book.expirydate.strftime('%d-%m-%Y')
+            issue_date = issued_book.issuedate.strftime('%d/%m/%Y')
+            expiration_date = issued_book.expirydate.strftime('%d/%m/%Y')
 
             days = (date.today() - issued_book.issuedate).days
-            fine = max((days - 15) * 10, 0) if days > 15 else 0
+            fine = max(0, (days - 15) * 10) if days > 15 else 0
 
             combined_data.append((
-                student.get_name, student.enrollment, student.branch,
-                book.name, book.author, issdate, expdate, fine, issued_book.status, issued_book.id
+                student.get_name,
+                student.enrollment,
+                student.branch,
+                book.name,
+                book.author,
+                issue_date,
+                expiration_date,
+                fine,
+                issued_book.status,
+                issued_book.id
             ))
 
     return render(request, 'library/viewissuedbookbystudent.html', {'combined_data': combined_data})
 
-def returnbook(_, id):
+
+def return_book(_, id):
     issued_book = models.IssuedBook.objects.get(pk=id)
     issued_book.status = "Returned"
     issued_book.save()
     return redirect('viewissuedbookbystudent')
 
+
 def aboutus_view(request):
-    return render(request,'library/aboutus.html')
+    return render(request, 'library/aboutus.html')
+
 
 def contactus_view(request):
     submit = forms.ContactusForm()
@@ -194,7 +205,8 @@ def contactus_view(request):
             message = submit.cleaned_data['Message']
 
             subject = f"{name} || {email}"
-            recipient_list = [os.getenv('RECIPIENT_EMAIL', 'zmarcoos12@gmail.com')]
+            recipient_list = [
+                os.getenv('RECIPIENT_EMAIL', 'zmarcoos12@gmail.com')]
 
             try:
                 send_mail(
--- librarymanagement/urls.py
+++ librarymanagement/urls.py
@@ -17,35 +17,33 @@ from django.contrib import admin
 from django.conf.urls import include
 from django.urls import path
 from library import views
-from django.contrib.auth.views import LoginView,LogoutView
-
-
+from django.contrib.auth.views import LoginView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('accounts/',include('django.contrib.auth.urls') ),
+    path('accounts/', include('django.contrib.auth.urls')),
     path('', views.home_view),
 
-    path('adminclick', views.adminclick_view),
-    path('studentclick', views.studentclick_view),
+    path('adminclick', views.admin_click_view),
+    path('studentclick', views.student_click_view),
 
+    path('studentsignup', views.student_signup_view),
+    path('adminlogin', LoginView.as_view(
+        template_name='library/adminlogin.html')),
+    path('studentlogin', LoginView.as_view(
+        template_name='library/studentlogin.html')),
+    path('returnbook/<int:id>/', views.return_book, name='returnbook'),
 
-    # path('adminsignup', views.adminsignup_view),
-    path('studentsignup', views.studentsignup_view),
-    path('adminlogin', LoginView.as_view(template_name='library/adminlogin.html')),
-    path('studentlogin', LoginView.as_view(template_name='library/studentlogin.html')),
-    path('returnbook/<int:id>/', views.returnbook, name='returnbook'),
+    path('logout', views.custom_logout_view, name='logout'),
+    path('afterlogin', views.after_login_view),
 
-
-    path('logout', LogoutView.as_view(template_name='library/index.html')),
-    path('afterlogin', views.afterlogin_view),
-
-    path('addbook', views.addbook_view),
-    path('viewbook', views.viewbook_view),
-    path('issuebook', views.issuebook_view),
-    path('viewissuedbook', views.viewissuedbook_view),
-    path('viewstudent', views.viewstudent_view),
-    path('viewissuedbookbystudent', views.viewissuedbookbystudent,name='viewissuedbookbystudent'),
+    path('addbook', views.add_book_view),
+    path('viewbook', views.view_book_view),
+    path('issuebook', views.issue_book_view),
+    path('viewissuedbook', views.view_issued_book_view),
+    path('viewstudent', views.view_student_view),
+    path('viewissuedbookbystudent', views.view_issued_book_by_student,
+         name='viewissuedbookbystudent'),
 
     path('aboutus', views.aboutus_view),
     path('contactus', views.contactus_view),
