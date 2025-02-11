5926d8a8db578a166f9a8db52f84050635e76e6b
zMarcoos <marcosvdcdef@gmail.com>
2025-02-11 01:18:24 -0300

    Melhorando a segurança

--- librarymanagement/settings.py
+++ librarymanagement/settings.py
@@ -34,7 +34,7 @@ load_dotenv()
 SECRET_KEY = os.getenv('DJANGO_SECRET_KEY', get_random_secret_key())
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['localhost', '127.0.0.1']
 
